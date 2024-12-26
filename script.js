53d91e356b0587aafa983e631330242579330c59
johnplakon <50604892+johnplakon@users.noreply.github.com>
2024-12-26 12:54:16 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for contacting Campaign Shield! We’ll get back to you shortly.');
-});
