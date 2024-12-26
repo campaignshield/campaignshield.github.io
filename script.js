9e872b6f721479f0c6316ce6dfc50d3e65199a6e
johnplakon <50604892+johnplakon@users.noreply.github.com>
2024-12-26 00:18:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
     e.preventDefault();
-    alert('Thank you for reaching out! We will get back to you soon.');
+    alert('Thank you for contacting Campaign Shield! We’ll get back to you shortly.');
 });
